e8601d15b2ef17ffdce119ed521447ba4a06d36d
LuckyTY127 <74975488+LuckyTY127@users.noreply.github.com>
2022-01-12 14:45:12 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ images.g.ts
@@ -0,0 +1,56 @@
+// Code généré automatiquement. Ne pas modifier.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "main chara":
+            case "anim1":return [img`
+. . . . f f f f . . . . . 
+. . f f f f f f f f . . . 
+. f f f f f b c f f f . . 
+f f f f f b c 3 f f f c . 
+f f 3 c f f f f f f f 3 . 
+c b 3 f f f e e f f 3 b . 
+f f f f e e e e e c b f . 
+f f f b f e e f b f f f . 
+. f 4 1 f 4 4 f 1 4 f . . 
+. f e 4 4 d d 4 4 e f . . 
+. f f f e e e e f f f . . 
+f e e 7 7 7 7 7 7 e e f . 
+f e 4 f 7 7 7 7 f 4 e f . 
+f e e f 6 5 5 6 f e e f . 
+f f f . f f f f . f f f . 
+. . . f f . . f f . . . . 
+`];
+            case "main chara0":
+            case "anim2":return [img`
+. . . . f f f f . . . . . 
+. . f f f f f f f f . . . 
+. f f f f f b c f f f . . 
+f f f f f b c 3 f f f c . 
+f f 3 c f f f f f f f 3 . 
+c b 3 f f f e e f f 3 b . 
+f f f f e e e e e c b f . 
+f f f b f e e f b f f f . 
+. f 4 1 f 4 4 f 1 4 f . . 
+. f e 4 4 d d 4 4 e f . . 
+. f f f e e e e f f f . . 
+f e e 7 7 7 7 7 7 e e f . 
+f e 4 f 7 7 7 7 f 4 e f . 
+f e e f 6 5 5 6 f e e f . 
+f f f . f f f f . f f f . 
+. . . f f . . f f . . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Code généré automatiquement. Ne pas modifier.
